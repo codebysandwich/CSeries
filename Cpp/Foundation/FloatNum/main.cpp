9eae3e59be77d8e24b3bfd0571b9039bcaa2b0e3
sandwich <122079260@qq.com>
2021-11-09 17:15:19 +0800

    submit float Precision

--- Cpp/Foundation/FloatNum/main.cpp
+++ Cpp/Foundation/FloatNum/main.cpp
@@ -7,8 +7,20 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    using namespace std;
+    // 不截断输出显示
+    cout.setf(ios_base::fixed, ios_base::floatfield);
+    float tub = 10.0 / 3.0;
+    double mint = 10.0 / 3.0;
+    const float million = 1.0e6;
+    
+    cout << "tub = " << tub
+         << ", a million tubs = " << million * tub << endl;
+    cout << "and ten millions tubs = " << 10 * million * tub << endl;
+    
+    cout << "mint = " << mint
+         << ", a million mints = " << million * mint << endl;
+    
     return 0;
 }
