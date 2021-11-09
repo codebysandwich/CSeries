c0e3ee9bca84d15812e4da17c001d98a4557d3df
sandwich <122079260@qq.com>
2021-11-09 17:13:37 +0800

    submit number stackover

--- Cpp/Foundation/Exceed/main.cpp
+++ Cpp/Foundation/Exceed/main.cpp
@@ -6,9 +6,35 @@
 //
 
 #include <iostream>
+#define ZERO 0
+#include <climits>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+/*
+ 数据溢出后的表现像是一个圈
+ */
+int main() {
+    using namespace std;
+    
+    short sam = SHRT_MAX;
+    unsigned short sue = sam;
+    
+    cout << "Sam has " << sam << " dollars and Sue has " << sue;
+    cout << " dollars deposited." << endl
+         << "Add $1 to each count." << endl << "Now ";
+    sam = sam + 1;
+    sue = sue + 1;
+    cout << "Sam has " << sam << " dollars and Sue has " << sue;
+    cout << " dollars deposited.\nPoor Sam!" << endl;
+    sam = ZERO;
+    sue = ZERO;
+    cout << "Sam has " << sam << " dollars and Sue has " << sue;
+    cout << " dollars deposited." << endl
+         << "Take $1 from each count." << endl << "Now ";
+    sam -= 1;
+    sue -= 1;
+    cout << "Sam has " << sam << " dollars and Sue has " << sue;
+    cout << " dollars depsited." << endl << "Lucky Sue!" << endl;
+    
+    
     return 0;
 }
